7ef9cc16ddffc750911c752df820a4af5df10353
Robert Heler <robertheler@gmail.com>
2020-05-19 09:40:00 -0700

    Refactor

--- docker.zsh
+++ docker.zsh
@@ -13,12 +13,17 @@ docker rm -f adidas_front_end_related_1
 docker rm -f proxy
 docker rm -f product
 docker rm -f adidas-product_web
-
+docker rm -f robertheler/fec-proxy
+docker rm -f robertheler/proxy
+docker rm -f adidas-product_web_1
+docker rm -f adidas-product_mongo_1
 #delete images
 docker rmi adidas_front_end_product
 docker rmi adidas_front_end_proxy
 docker rmi adidas_front_end_related
 docker rmi -f adidas-product_web
+docker rmi -f adidas-product_web_1
 docker rmi -f robertheler/fec-solo
-
+docker rmi -f robertheler/proxy
+docker rmi -f robertheler/fec-proxy
 #docker-compose up
